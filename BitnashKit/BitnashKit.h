3c9c70afbd8ad85793f7555c1537238610e7f7ab
Rich Collins <richcollins@gmail.com>
2014-03-08 14:31:00 -0800

    Initial Commit

---
+++ BitnashKit/BitnashKit.h
@@ -0,0 +1,13 @@
+//
+//  BitnashKit.h
+//  BitnashKit
+//
+//  Created by Rich Collins on 3/8/14.
+//  Copyright (c) 2014 Bitmarkets. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BitnashKit : NSObject
+
+@end
---
+++ BitnashKit/BitnashKit.m
@@ -0,0 +1,13 @@
+//
+//  BitnashKit.m
+//  BitnashKit
+//
+//  Created by Rich Collins on 3/8/14.
+//  Copyright (c) 2014 Bitmarkets. All rights reserved.
+//
+
+#import "BitnashKit.h"
+
+@implementation BitnashKit
+
+@end
