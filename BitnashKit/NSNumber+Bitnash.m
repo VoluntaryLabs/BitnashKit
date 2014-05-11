df8a240d46b1835a992e0000badd3cad1e4a8846
Steve Dekorte <steve@dekorte.com>
2014-05-11 15:25:40 -0700

    NSNumber category for btc to satoshi conversion

---
+++ BitnashKit/NSNumber+Bitnash.m
@@ -0,0 +1,31 @@
+//
+//  NSNumber+Bitnash.m
+//  BitnashKit
+//
+//  Created by Steve Dekorte on 5/11/14.
+//  Copyright (c) 2014 Bitmarkets. All rights reserved.
+//
+
+#import "NSNumber+Bitnash.h"
+
+@implementation NSNumber (Bitnash)
+
++ (double)satoshiPerBtc
+{
+    return 100000000.0;
+}
+
+- (NSNumber *)btcToSatoshi
+{
+    long long satoshi = (long long)(self.doubleValue * self.class.satoshiPerBtc);
+    return [NSNumber numberWithLongLong:satoshi];
+}
+
+- (NSNumber *)satoshiToBtc
+{
+    long long satoshi = self.longLongValue;
+    double btc = satoshi / self.class.satoshiPerBtc;
+    return [NSNumber numberWithDouble:btc];
+}
+
+@end
