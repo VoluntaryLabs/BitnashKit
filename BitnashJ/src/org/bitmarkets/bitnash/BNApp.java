d097fe753e34e8351039ef477a7cd45d5ef2e590
Rich Collins <richcollins@gmail.com>
2014-05-24 12:00:59 -0700

    Always copy BitnashJ to resource so clean isn't needed.

--- BitnashJ/src/org/bitmarkets/bitnash/BNApp.java
+++ BitnashJ/src/org/bitmarkets/bitnash/BNApp.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 
 public class BNApp {
 	public static void main(String[] args) throws IOException {
-		
+		System.err.println("AFTER");
 		if (args.length == 1) {
 			BNWallet.shared().getWalletAppKit().setCheckpoints(new FileInputStream(new File(args[0])));
 		}
